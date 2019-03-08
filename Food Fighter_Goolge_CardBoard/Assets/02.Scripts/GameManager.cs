a6e7c5317c1412ddeea125b46536b2e8fb4e3775
UnityHJ <48305432+UnityHJ@users.noreply.github.com>
2019-03-08 12:20:20 +0900

    delete 100mb files
    C:\HJ\UnityTeamPoroject_FF\Food Fighter_Goolge_CardBoard\Library\PlayerDataCache\Android\Data
    
    removed sharedassets0.resource

---
+++ Food Fighter_Goolge_CardBoard/Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Bread Create Info")]
+    public Transform[] points;
+    public GameObject[] bread;
+    public float createTime = 1.0f;
+    public int maxBread = 20;
+    public bool isGameOver = false;
+    public static GameManager instance = null;
+    public List<GameObject> breadPool = new List<GameObject>();
+
+    public int randomIdx;
+
+
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != null) Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);        
+    }
+
+
+    void Start()
+    {
+        points = GameObject.Find("SpawnPointGroup").GetComponentsInChildren<Transform>();
+        if (points.Length > 0)
+        {
+            StartCoroutine(CreateBread());
+        }
+
+    }
+
+    private IEnumerator CreateBread()
+    {
+        while (!isGameOver)
+        {
+            int breadCount = GameObject.FindGameObjectsWithTag("Bread").Length;
+            if (breadCount < maxBread)
+            {
+                yield return new WaitForSeconds(createTime);
+                if (isGameOver) break;
+                int idx = Random.Range(1, points.Length);
+                randomIdx = Random.Range(0, bread.Length);
+                Instantiate(bread[randomIdx], points[idx].position, points[idx].rotation);
+                for (int i = 0; i < breadPool.Count; i++)
+                {
+                    if (breadPool[i].activeSelf == false)
+                    {
+                        breadPool[i].tag = "Bread";
+                        breadPool[i].transform.position = points[idx].position;
+                        breadPool[i].transform.rotation = points[idx].rotation;
+                        breadPool[i].SetActive(true);
+                        break;
+                    }
+                }
+            }
+            else yield return null;
+        }
+    }
+}
---
+++ Food Fighter_Goolge_CardBoard/Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+
+    //private float h = 0f;
+    private float v = 0f;
+    private float r = 0f;
+    private Transform tr;
+
+    private float rotationangley1;
+    private float rotationangley2;
+
+    public float rotSpeed = 90.0f;
+    public float viewAngle = 90.0f;
+    public float _maxAngle = 45f;
+    public float _minAngle = -45f;
+
+    
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        Vector3 axis = tr.position;
+    }
+
+    
+    void Update()
+    {        
+        v = Input.GetAxis("Mouse Y");
+        r = Input.GetAxis("Mouse X");
+
+        //rotationangley1 = transform.eulerAngles.y;
+        //rotationangley2 = transform.eulerAngles.y;
+
+        //if (rotationangley1 > 330 && rotationangley1 < 340)
+        //{
+
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, 10, transform.eulerAngles.z),
+        //        Time.deltaTime * 1.5f);
+        //    tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r);
+        //}
+
+        //if (rotationangley2 > 30 && rotationangley2 < 40)
+        //{
+
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(transform.eulerAngles.x, -10, transform.eulerAngles.z),
+        //        Time.deltaTime * 1.5f);
+        //    tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r);
+        //}
+
+        tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r);
+        tr.Rotate(Vector3.right * rotSpeed * Time.deltaTime * v);
+
+    }
+
+}
