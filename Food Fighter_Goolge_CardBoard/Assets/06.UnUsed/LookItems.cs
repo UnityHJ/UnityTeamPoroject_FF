8455f2ef3fd18386132250a27e916532554f977e
UnityHJ <48305432+UnityHJ@users.noreply.github.com>
2019-03-11 00:17:43 +0900

    ReUpload All the files

---
+++ Food Fighter_Goolge_CardBoard/Assets/06.UnUsed/LookItems.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LookItems : MonoBehaviour, IPointerEnterHandler
+    , IPointerExitHandler, IGvrPointerHoverHandler
+{
+    
+
+    public void OnGvrPointerHover(PointerEventData eventData)
+    {
+        //Debug.Log("Pointer Hover!!!");
+        
+    }
+
+    public void OnLookItemBox(bool isLookAt)
+    {
+        Debug.Log(isLookAt);
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        
+        Debug.Log("Enter!!!");
+        
+        SendMessage("ItemIsSelected", SendMessageOptions.DontRequireReceiver);
+            
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        
+    }
+
+    //public void movingChick()
+    //{
+    //    var trPos = GameObject.FindGameObjectWithTag("FrontPosition");
+
+    //    this.gameObject.transform.Translate(trPos.transform.position);
+
+    //    //yield return new WaitForSeconds(2.0f);
+
+
+    //}
+
+
+}
+
+
