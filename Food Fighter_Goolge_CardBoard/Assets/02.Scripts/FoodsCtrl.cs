251c793e3b73e1a0eea17df246dca4df5b12f128
aiguya <aiguya180@gmail.com>
2019-03-21 18:05:27 +0900

    치킨 모델링 수정 중
    치킨 모델링 수정 중
    한입씩 먹는 효과 적용 중
    콜라 드링크 적용

--- Food Fighter_Goolge_CardBoard/Assets/02.Scripts/FoodsCtrl.cs
+++ Food Fighter_Goolge_CardBoard/Assets/02.Scripts/FoodsCtrl.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class FoodsCtrl : MonoBehaviour
 {
     private const string TAG = "FoodsCtrl";
-    private Transform eatingTr;
+    private Transform chickTr;
     private Transform drinkingTr;
+    private Transform eatingTr;
     private Vector3 originPos;
     private Rigidbody rb;
+    private MeshFilter _meshFilter;
+    private MeshRenderer _renderer;
+    private MeshCollider _meshColl;
 
     public float speed = 5.0f;
+    public Mesh[] meshs;
+    public Material[] textures;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        eatingTr = GameObject.Find("EatPos").GetComponent<Transform>();
+        chickTr = GameObject.Find("ChickPos").GetComponent<Transform>();
         drinkingTr = GameObject.Find("DrinkPos").GetComponent<Transform>();
+        eatingTr = GameObject.Find("EatPos").GetComponent<Transform>();
+        _meshFilter = GetComponent<MeshFilter>();
+        _renderer = GetComponent<MeshRenderer>();
+        _meshColl = GetComponent<MeshCollider>();
     }
 
     public void OnClick()
@@ -33,7 +43,7 @@ public class FoodsCtrl : MonoBehaviour
         rb.isKinematic = true;
         while (GameManager.Instance.itemState == ItemState.MOVING)
         {
-            Transform tr = eatingTr;
+            Transform tr = chickTr;
             if (tag == "COKE")
             {
                 tr = drinkingTr;
@@ -71,8 +81,35 @@ public class FoodsCtrl : MonoBehaviour
         Debug.Log(TAG + " [EatChicken]");
         for (int i = 0; i < 3; i++)
         {
+            while (true)
+            {
+                transform.position = Vector3.Lerp(transform.position, eatingTr.position, Time.deltaTime * speed * 1.5f);
+                float dis = Vector3.Distance(transform.position, eatingTr.position);
+
+                if(dis < 0.005f)
+                {
+                    transform.position = eatingTr.position;
+                    break;
+                }
+                yield return null;
+            }
             float audioTime = SoundCtrl.Instance.MakeSounds(SoundEffects.BITE);
-            yield return new WaitForSeconds(audioTime);
+            yield return new WaitForSeconds(audioTime * 0.5f);
+            _renderer.material = textures[i];
+            _meshFilter.sharedMesh = meshs[i];
+            _meshColl.sharedMesh = meshs[i];
+            transform.rotation = chickTr.rotation;
+            while (true)
+            {
+                transform.position = Vector3.Lerp(transform.position, chickTr.position, Time.deltaTime * speed * 1.5f);
+                float dis = Vector3.Distance(transform.position, chickTr.position);
+                if (dis < 0.001f)
+                {
+                    transform.position = chickTr.position;
+                    break;
+                }
+                yield return null;
+            }
             audioTime = SoundCtrl.Instance.MakeSounds(SoundEffects.CHEW);
             GameManager.Instance.EatSome(5.0f);
             if (i == 2)
@@ -88,15 +125,26 @@ public class FoodsCtrl : MonoBehaviour
     private IEnumerator Drink()
     {
         Debug.Log(TAG + " [Drink]");
-        float audioTime = SoundCtrl.Instance.MakeSounds(SoundEffects.DRINK);
+        for (int i = 0; i < 3; i++)
+        {
+            float audioTime = SoundCtrl.Instance.MakeSounds(SoundEffects.DRINK);
+            yield return new WaitForSeconds(audioTime + 0.5f);
+        }
         GameManager.Instance.DrinkSome(3.0f);
-        yield return new WaitForSeconds(audioTime);
+        SoundCtrl.Instance.MakeSounds(SoundEffects.BURP);
         GameManager.Instance.itemState = ItemState.NORMAL;
-        float dis = Vector3.Distance(transform.position, originPos);
+
         while (true)
         {
-            transform.position = Vector3.Lerp(transform.position, originPos, Time.deltaTime * speed * 2);
+            transform.position = Vector3.Lerp(transform.position, originPos, Time.deltaTime * speed);
             yield return null;
+            float dis = Vector3.Distance(transform.position, originPos);
+            if (dis < 0.001f)
+            {
+                transform.position = originPos;
+                Debug.Log(TAG + " [Drink] break");
+                break;
+            }
         }
 
     }
